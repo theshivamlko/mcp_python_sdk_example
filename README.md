0de908433f59e53d3548873a38e9eea597a2da01
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2025-11-24 04:27:33 +0530

    server working

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+### MCP Inspector
+
+Run </br>
+`uv run mcp dev server.py:notes_mcp`
--- server.py
+++ server.py
@@ -9,12 +9,12 @@ from mcp.server import FastMCP
 import json
 from pathlib import Path
 
-notes_mcp = FastMCP(name="Note MCP Server", description="A note taking application")
+notes_mcp = FastMCP(name="Note MCP Server")
 
 
 # create a file my_notes.json that contains a list of notes
 
-NOTES_FILE=Path.home()/"my_notes.json"
+NOTES_FILE="my_notes.json"
 
 def load_notes()-> dict:
     notes_path = Path(NOTES_FILE)
@@ -24,16 +24,20 @@ def load_notes()-> dict:
     return {}
 
 def save_notes(notes: dict):
-    NOTES_FILE.write_text(json.dump(notes,indent=2))
+    notes_path = Path(NOTES_FILE)
+    with open(notes_path, "r") as f:
+        f.write(json.dumps(notes, indent=2))
+        # NOTES_FILE.write_text(json.dump(notes,indent=2))
 
-
+@notes_mcp.tool()
 def add_note(name: str, content: str) -> str:
     notes=load_notes()
+    print( notes)
     notes[name]=content
     save_notes(notes)
     return f"Note '{name}' added!"
 
-
+@notes_mcp.tool()
 def delete_note(name: str) -> str:
     notes=load_notes()
     if name in notes:
@@ -43,7 +47,7 @@ def delete_note(name: str) -> str:
     else:
         return f"Note '{name}' does not exist."
     
-
+@notes_mcp.tool()
 def get_note(name: str) -> str:
     notes=load_notes()
     if name in notes:
@@ -52,10 +56,27 @@ def get_note(name: str) -> str:
         return f"Note '{name}' does not exist."
     
     
-    
+@notes_mcp.tool()
 def list_notes() -> str:
     notes=load_notes()
     if notes:
         return "\n".join(notes.keys())
     else:
         return "No notes available."
+
+
+
+@notes_mcp.resource("resource://{name}")
+def get_note(name: str) -> str:
+    notes=load_notes()
+    if name in notes:
+        return notes[name]
+    else:
+        return f"Note '{name}' does not exist."
+
+@notes_mcp.prompt()
+def summarize_note(name: str) -> str:
+    notes=load_notes()
+    if name in notes:
+        return f"Here is the note summarize it in 10 words or less:\n\n{notes[name]}"
+    return f"Note '{name}' nto found."
